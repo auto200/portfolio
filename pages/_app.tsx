ba980d3af51529b8c444c684a4b02837ed8ff223
auto200 <michal.warac@gmail.com>
2021-04-14 18:15:10 +0200

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import theme from "../utils/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Michosz to ja</title>
+        <meta property="og:title" content="Michosz to ja" key="title" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
