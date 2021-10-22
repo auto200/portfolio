7b7026342a0245e3274b564f799a3fbdb03e49c0
auto200 <michal.warac@gmail.com>
2021-10-22 19:20:24 +0200

    Add projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import { Box, VStack } from "@chakra-ui/react";
 import Hero from "@components/sections/Hero";
+import Projects from "@components/sections/Projects";
+import React from "react";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <VStack>
+        <Box w="100%" maxW="900px">
+          <Projects />
+        </Box>
+      </VStack>
     </>
   );
 };
